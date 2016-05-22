402617f48b493fd68321bc57199d487fde0ab6d2
Robert Hardy <robbert@gmail.com>
2016-05-22 18:01:27 +0100

    Modify test to check number of non-trivial sums.
    Fails, wip.

--- sum_of_cubes.py
+++ sum_of_cubes.py
@@ -1,10 +1,15 @@
 def find_one_pair():
-    lst = range(100)
-    all_sums = [ a^3 + b^3 for a in lst for b in lst ]
-    all_sums = sorted(all_sums)
+    triangle = create_triangle()
+    all_sums = [ a^3 + b^3 for (a, b) in triangle ]
     count = dict()
     for x in all_sums:
         if x in count:
-            return x
-        count[x] = 1
-    return None
+            count[x] = count[x] + 1
+        else:
+            count[x] = 1
+    return { x: count[x] for x in count if count[x] != 1 }
+
+def create_triangle():
+    lst = range(1000)
+    triangle = [ (a, b) for a in lst for b in lst if b <= a ]
+    return triangle
--- test/test_sum_of_cubes.py
+++ test/test_sum_of_cubes.py
@@ -1,8 +1,12 @@
 import unittest
 
-from sum_of_cubes import find_one_pair
+from sum_of_cubes import find_one_pair, create_triangle
 
 class TestSumOfCubes(unittest.TestCase):
     def test_can_find_one_under_one_thousand(self):
         result = find_one_pair();
-        self.assertTrue(result is not None)
+        self.assertEqual(len(result.keys()), 10)
+
+    def test_triangle(self):
+        result = create_triangle()
+        self.assertEqual(len(result), 500500)
