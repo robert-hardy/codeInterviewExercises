3861c327f8109284953f779eebcee237ce9a482e
Robert Hardy <robbert@gmail.com>
2016-05-22 18:04:11 +0100

    Test green.

--- test/test_sum_of_cubes.py
+++ test/test_sum_of_cubes.py
@@ -5,7 +5,7 @@ from sum_of_cubes import find_one_pair, create_triangle
 class TestSumOfCubes(unittest.TestCase):
     def test_can_find_one_under_one_thousand(self):
         result = find_one_pair();
-        self.assertEqual(len(result.keys()), 10)
+        self.assertEqual(len(result.keys()), 1024)
 
     def test_triangle(self):
         result = create_triangle()
