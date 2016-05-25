17842a516e09e94ec809acc971aeae08de882000
Robert Hardy <robbert@gmail.com>
2016-05-25 17:12:28 +0100

    Add li class to hold each item. Test get value. Test get next value.
    One test red, wip.

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,26 @@
 class ll(object):
     def __init__(self, lst):
-        self.head = lst[0]
+        final_node = li(None, None)
+        current_node = final_node
+        for x in lst:
+            new_node = li(x, current_node)
+            current_node = new_node
+        self.head = current_node
 
     def value(self):
-        return self.head
+        return self.head.value()
+
+    def next(self):
+        next_li = self.head.next()
+        self.head = next_li
+
+class li(object):
+    def __init__(self, value, ni):
+        self.val = value
+        self.ni = ni
+
+    def value(self):
+        return self.val
+
+    def next(self):
+        return self.ni
--- test/test_linked_list.py
+++ test/test_linked_list.py
@@ -1,13 +1,25 @@
 import unittest
 
-from linked_list import ll
+from linked_list import ll, li
 
 class TestLL(unittest.TestCase):
     def setUp(self):
         self.result = ll("abcdef")
 
     def test_get_head(self):
-        self.assertEquals(self.result.value(), 'a')
+        self.assertEquals(self.result.value(), 'f')
 
     def test_get_next(self):
+        import ipdb; ipdb.set_trace()
         self.assertEquals(self.result.next().value())
+
+class TestLI(unittest.TestCase):
+    def test_get_value(self):
+        result = li('a', None)
+        self.assertEqual(result.value(), 'a')
+
+    def test_get_next_value(self):
+        foo = li('a', None)
+        bar = li('b', foo)
+        self.assertEqual(bar.value(), 'b')
+        self.assertEqual(bar.next().value(), 'a')
