f38414c28211d9218690d920288b496f8c9faa74
Robert Hardy <robbert@gmail.com>
2016-05-26 19:52:35 +0000

    Test and implement sum function.
    All green.

--- test/test_two_sum.py
+++ test/test_two_sum.py
@@ -1,6 +1,9 @@
 import unittest
 
-from two_sum import all_permutations
+from two_sum import (
+    all_permutations,
+    get_first_sum_to_target
+)
 
 class TestFoo(unittest.TestCase):
     def test_all_permutations(self):
@@ -14,3 +17,15 @@ class TestFoo(unittest.TestCase):
                 ((1, 'b'), (2, 'c'))
             ]
         )
+
+    def test_sum_function(self):
+        combos = [
+            ((0, 1), (1, 2)),
+            ((0, 1), (2, 3)),
+            ((1, 2), (2, 3))
+        ]
+        result = get_first_sum_to_target(combos, 4)
+        self.assertEqual(result, ((0, 1), (2, 3)))
+
+        result = get_first_sum_to_target(combos, 7)
+        self.assertEqual(result, None)
--- two_sum.py
+++ two_sum.py
@@ -2,3 +2,9 @@ import itertools
 
 def all_permutations(lst):
     return list(itertools.combinations(enumerate(lst), 2))
+
+def get_first_sum_to_target(combos, target):
+    for (x, y) in combos:
+        if x[1] + y[1] == target:
+            return (x, y)
+    return None
