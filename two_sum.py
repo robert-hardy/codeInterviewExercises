1ea5bf6aaddd2237e04fc770da365eb40f37de2d
Robert Hardy <robbert@gmail.com>
2016-05-26 20:23:39 +0000

    Add __main__.

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,5 @@
 import itertools
+from random import shuffle
 
 def all_permutations(lst):
     return list(itertools.combinations(enumerate(lst), 2))
@@ -8,3 +9,9 @@ def get_first_sum_to_target(combos, target):
         if x[1] + y[1] == target:
             return (x, y)
     return None
+
+if __name__ == '__main__':
+    lst = list(range(10))
+    shuffle(lst)
+    print lst
+    print get_first_sum_to_target(all_permutations(lst), 5)
