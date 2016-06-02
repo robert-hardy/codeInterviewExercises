3778530af7031c630381af83c5367963803da2a2
Robert Hardy <robbert@gmail.com>
2016-06-02 15:02:35 +0000

    Set points to NULL.
    All green.

--- cpp/binary_tree.cpp
+++ cpp/binary_tree.cpp
@@ -4,6 +4,8 @@ void add(node *n, int val) {
     if (!n->left) {
         node *new_node = new node;
         new_node->value = val;
+        new_node->left = 0;
+        new_node->right = 0;
         n->left = new_node;
     }
 }
--- cpp/test_binary_tree.cpp
+++ cpp/test_binary_tree.cpp
@@ -4,6 +4,8 @@
 TEST(TestNode, CreateSmallTree) {
     node *root = new node;
     root->value = 1;
+    root->left = 0;
+    root->right = 0;
     add(root, 2);
-    ASSERT_EQ(root->left->value, 3);
+    ASSERT_EQ(root->left->value, 2);
 }
