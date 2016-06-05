dd12c2782b81488c69b3f2f4711663cf27545cbb
Robert Hardy <robbert@gmail.com>
2016-06-05 10:15:23 +0000

    Initial commit of cpp linked list.
    All green.

---
+++ cpp/linked_list.hpp
@@ -0,0 +1,11 @@
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
+#include <string>
+#include <vector>
+
+struct li;
+
+li *make_list(std::vector<std::string>);
+
+#endif /* LINKEDLIST_HPP */
---
+++ cpp/test/test_linked_list.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+#include "../linked_list.hpp"
+
+TEST(LinkedList, GettingStarted) {
+    ASSERT_EQ("foo", "foo");
+}
