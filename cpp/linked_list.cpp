26e72920bb23bce419e28f17c07f0b7d268066a5
Robert Hardy <robbert@gmail.com>
2016-06-05 11:02:43 +0000

    Fix, need to check the length of the vector.
    All green.

--- cpp/linked_list.cpp
+++ cpp/linked_list.cpp
@@ -6,6 +6,8 @@ li::li(std::string val) :
 
 li *make_list(std::vector<std::string> values) {
     li *result = new li(values[0]);
-    result->next = new li(values[1]);
+    if (values.size() > 1) {
+        result->next = new li(values[1]);
+    }
     return result;
 }
