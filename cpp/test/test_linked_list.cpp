0f8580a2421ce04f70dcb815157b4910c236aa07
Robert Hardy <robbert@gmail.com>
2016-06-05 10:25:13 +0000

    Test we can initialize a vector of strings.
    All green.

--- cpp/test/test_linked_list.cpp
+++ cpp/test/test_linked_list.cpp
@@ -2,6 +2,9 @@
 
 #include "../linked_list.hpp"
 
-TEST(LinkedList, GettingStarted) {
-    ASSERT_EQ("foo", "foo");
+TEST(LinkedList, CheckCanInitializeVector) {
+    const char *init[] = {"hello", "world"};
+    std::vector<std::string> v(init, init + sizeof(init)/sizeof(init[0]));
+    ASSERT_EQ(v[0], "hello");
+    ASSERT_EQ(v[1], "world");
 }
