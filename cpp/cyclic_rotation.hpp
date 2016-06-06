3c0f3b3b6794a74eae2cd25a76ec9b64943a92f8
Robert Hardy <robbert@gmail.com>
2016-06-06 10:55:53 +0000

    Integrate files for the CyclicRotation exercise.
    Test red, just to prove the test is building.

---
+++ cpp/cyclic_rotation.hpp
@@ -0,0 +1,8 @@
+#ifndef CYCLICROTATION_HPP
+#define CYCLICROTATION_HPP
+
+#include <vector>
+
+std::vector<int> solution(std::vector<int> &A, int K);
+
+#endif /* CYCLICROTATION_HPP */
---
+++ cpp/test/test_cyclic_rotation.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+#include "../cyclic_rotation.hpp"
+
+TEST(CyclicRotation, GettingStarted) {
+    ASSERT_EQ("13", "12");
+}
