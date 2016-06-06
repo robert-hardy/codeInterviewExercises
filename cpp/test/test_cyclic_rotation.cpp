821f2cf4bd7fffd78292f0e9a023092c86e534fd
Robert Hardy <robbert@gmail.com>
2016-06-06 10:58:42 +0000

    Test we can initialize the vector.
    All green.

--- cpp/test/test_cyclic_rotation.cpp
+++ cpp/test/test_cyclic_rotation.cpp
@@ -2,6 +2,9 @@
 
 #include "../cyclic_rotation.hpp"
 
-TEST(CyclicRotation, GettingStarted) {
-    ASSERT_EQ("13", "12");
+TEST(CyclicRotation, CheckInitializationOfVector) {
+    int init[] = {1, 2, 3, 4};
+    std::vector<int> v(init, init + sizeof(init)/sizeof(init[0]));
+    ASSERT_EQ(v.front(), 1);
+    ASSERT_EQ(v.back(), 4);
 }
