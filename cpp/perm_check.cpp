d0e9395a766a24ad8282c16f972fb7b422b30c5b
Robert Hardy <robbert@gmail.com>
2016-06-06 14:32:44 +0000

    Minor, indentation change.

--- cpp/perm_check.cpp
+++ cpp/perm_check.cpp
@@ -1,16 +1,18 @@
 #include "perm_check.hpp"
 
 namespace PermCheck {
-    int solution(std::vector<int> &A) {
-        int N = A.size();
-        int total_sum = 0;
-        std::vector<int>::iterator it = A.begin();
-        for (; it< A.end(); it++) {
-            total_sum += *it;
-        }
-        if (total_sum != N*(N+1)/2) {
-            return 0;
-        }
-        return 1;
+
+int solution(std::vector<int> &A) {
+    int N = A.size();
+    int total_sum = 0;
+    std::vector<int>::iterator it = A.begin();
+    for (; it< A.end(); it++) {
+        total_sum += *it;
     }
+    if (total_sum != N*(N+1)/2) {
+        return 0;
+    }
+    return 1;
+}
+
 }
