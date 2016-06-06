354d9eabb9ca6bb5820c75c757bcb8f5478a8783
Robert Hardy <robbert@gmail.com>
2016-06-06 12:03:37 +0000

    Make it look more like the expected loop.
    All green.

--- cpp/frog_river_one.cpp
+++ cpp/frog_river_one.cpp
@@ -1,5 +1,12 @@
 #include "frog_river_one.hpp"
 
 int solution(int X, int A[], int N) {
-    return 1;
+    if (X == 1) {
+        for (int i=0; i<N; i++) {
+            if (A[i] == 1) {
+                return i;
+            }
+        }
+    }
+    return -1;
 }
--- cpp/test/test_frog_river_one.cpp
+++ cpp/test/test_frog_river_one.cpp
@@ -5,5 +5,5 @@
 TEST(FrogRiverOne, TestRiverWide1) {
     int A[] = {1, 3, 1, 4, 2, 3, 5, 4};
     int result = solution(1, A, 8);
-    ASSERT_EQ(result, 1);
+    ASSERT_EQ(result, 0);
 }
