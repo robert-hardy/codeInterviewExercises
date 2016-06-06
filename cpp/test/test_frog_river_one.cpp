f7fa2308f8b8773e251217a302d8ee891abf623a
Robert Hardy <robbert@gmail.com>
2016-06-06 12:09:49 +0000

    Add another test, where result is not 0.
    All green.

--- cpp/test/test_frog_river_one.cpp
+++ cpp/test/test_frog_river_one.cpp
@@ -7,3 +7,9 @@ TEST(FrogRiverOne, TestRiverWide1) {
     int result = solution(1, A, 8);
     ASSERT_EQ(result, 0);
 }
+
+TEST(FrogRiverOne, TestRiverWide1NotFirstIndex) {
+    int A[] = {4, 3, 1, 4, 2, 3, 5, 4};
+    int result = solution(1, A, 8);
+    ASSERT_EQ(result, 2);
+}
