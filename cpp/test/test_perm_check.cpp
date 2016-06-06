35dc54d0058c1598dc2623ceb277c5444c030c8d
Robert Hardy <robbert@gmail.com>
2016-06-06 14:27:15 +0000

    Add more test cases. Add large test, without and with shuffling.
    All green.

--- cpp/test/test_perm_check.cpp
+++ cpp/test/test_perm_check.cpp
@@ -2,9 +2,34 @@
 
 #include "../perm_check.hpp"
 
-TEST(PermCheck, CheckTrueCase) {
+#include <algorithm>
+#include <numeric>
+
+TEST(PermCheck, CheckTrueCase1) {
     int init[] = {1, 2, 3, 4};
     std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
+    int result = PermCheck::solution(A);
+    ASSERT_EQ(result, 1);
+}
+
+TEST(PermCheck, CheckTrueCase2) {
+    int init[] = {1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
+    int result = PermCheck::solution(A);
+    ASSERT_EQ(result, 1);
+}
+
+TEST(PermCheck, CheckTrueCaseBigArray) {
+    std::vector<int> A(1000, 1);
+    std::partial_sum(A.begin(), A.end(), A.begin());
+    int result = PermCheck::solution(A);
+    ASSERT_EQ(result, 1);
+}
+
+TEST(PermCheck, CheckTrueCaseBigArrayShuffled) {
+    std::vector<int> A(1000, 1);
+    std::partial_sum(A.begin(), A.end(), A.begin());
+    std::random_shuffle(A.begin(), A.end());
     int result = PermCheck::solution(A);
     ASSERT_EQ(result, 1);
 }
