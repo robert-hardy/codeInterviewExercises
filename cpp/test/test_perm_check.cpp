fae83bc9ff727524963359b251721e08b66e97f2
Robert Hardy <robbert@gmail.com>
2016-06-06 14:01:30 +0000

    Check initialization.
    All green.

--- cpp/test/test_perm_check.cpp
+++ cpp/test/test_perm_check.cpp
@@ -2,6 +2,9 @@
 
 #include "../perm_check.hpp"
 
-TEST(PermCheck, GettingStarted) {
-    ASSERT_EQ(1, 2);
+TEST(PermCheck, CheckInitialization) {
+    int init[] = {1, 2, 3, 4};
+    std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
+    ASSERT_EQ(A.front(), 1);
+    ASSERT_EQ(A.back(), 4);
 }
