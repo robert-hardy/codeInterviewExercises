67bcd1fca9198e87066c4b9e71cb3a1a65318c5a
Robert Hardy <robbert@gmail.com>
2016-06-06 12:14:51 +0000

    All green.

--- cpp/frog_river_one.cpp
+++ cpp/frog_river_one.cpp
@@ -1,15 +1,22 @@
 #include "frog_river_one.hpp"
 
-int min_index(int target, int A[], int N);
+#include <algorithm>
+
+int first_occurrence(int target, int A[], int N);
 
 int solution(int X, int A[], int N) {
     if (X == 1) {
-        return min_index(1, A, N);
+        return first_occurrence(1, A, N);
+    }
+    if (X == 2) {
+        int first_occurrence_of_1 = first_occurrence(1, A, N);
+        int first_occurrence_of_2 = first_occurrence(2, A, N);
+        return std::max(first_occurrence_of_1, first_occurrence_of_2);
     }
     return -1;
 }
 
-int min_index(int target, int A[], int N) {
+int first_occurrence(int target, int A[], int N) {
     for (int i=0; i<N; i++) {
         if (A[i] == target) {
             return i;
