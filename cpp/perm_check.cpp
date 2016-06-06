6dbbbc98010ec1e5ee157b7063093da896909b46
Robert Hardy <robbert@gmail.com>
2016-06-06 14:46:34 +0000

    All green.

--- cpp/perm_check.cpp
+++ cpp/perm_check.cpp
@@ -4,13 +4,19 @@ namespace PermCheck {
 
 int solution(std::vector<int> &A) {
     int N = A.size();
-    int total_sum = 0;
+    std::vector<bool> seen(N, false);
     std::vector<int>::iterator it = A.begin();
-    for (; it< A.end(); it++) {
-        total_sum += *it;
+    for (; it < A.end(); it++) {
+        if (seen[*it-1]) {
+            return 0;
+        }
+        seen[*it-1] = true;
     }
-    if (total_sum != N*(N+1)/2) {
-        return 0;
+    std::vector<bool>::iterator it2 = seen.begin();
+    for (; it2 < seen.end(); it2++) {
+        if (!*it2) {
+            return 0;
+        }
     }
     return 1;
 }
--- cpp/test/test_perm_check.cpp
+++ cpp/test/test_perm_check.cpp
@@ -45,5 +45,23 @@ TEST(PermCheck, CheckFalseCaseWithCorrectTotalSum) {
     int init[] = {1, 2, 2, 5};
     std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
     int result = PermCheck::solution(A);
-    ASSERT_EQ(result, 0);
+    EXPECT_EQ(result, 0);
+    int xor_result = 0;
+    std::vector<int>::iterator it = A.begin();
+    for (; it<A.end(); it++) {
+        xor_result = xor_result ^ (1 + std::distance(A.begin(), it)) ^ *it;
+    }
+    ASSERT_EQ(xor_result, 0);
+}
+
+TEST(PermCheck, CheckXORStuff) {
+    std::vector<int> A(1000, 1);
+    std::partial_sum(A.begin(), A.end(), A.begin());
+    std::random_shuffle(A.begin(), A.end());
+    int xor_result = 0;
+    std::vector<int>::iterator it = A.begin();
+    for (; it<A.end(); it++) {
+        xor_result = xor_result ^ (1 + std::distance(A.begin(), it)) ^ *it;
+    }
+    ASSERT_EQ(xor_result, 0);
 }
