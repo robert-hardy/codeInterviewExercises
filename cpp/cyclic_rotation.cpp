b21460ea67dcd3c8cfd6529a7dcaf6f8e505d70c
Robert Hardy <robbert@gmail.com>
2016-06-06 11:15:12 +0000

    Test we can rotate by 1.
    All green.

--- cpp/cyclic_rotation.cpp
+++ cpp/cyclic_rotation.cpp
@@ -1,5 +1,12 @@
 #include "cyclic_rotation.hpp"
 
 std::vector<int> solution(std::vector<int> &A, int K) {
-    return A;
+    std::vector<int> result(A);
+    if (K == 0) {
+        return result;
+    }
+    int buffer = result.back();
+    result.insert(result.begin(), buffer);
+    result.erase(result.end() - 1);
+    return result;
 }
--- cpp/test/test_cyclic_rotation.cpp
+++ cpp/test/test_cyclic_rotation.cpp
@@ -5,16 +5,28 @@
 TEST(CyclicRotation, CheckInitializationOfVector) {
     int init[] = {1, 2, 3, 4};
     std::vector<int> v(init, init + sizeof(init)/sizeof(init[0]));
-    ASSERT_EQ(v.front(), 1);
-    ASSERT_EQ(v.back(), 4);
+    EXPECT_EQ(v.front(), 1);
+    EXPECT_EQ(v.back(), 4);
 }
 
 TEST(CyclicRotation, RotateByZero) {
     int init[] = {1, 2, 3, 4};
     std::vector<int> v(init, init + sizeof(init)/sizeof(init[0]));
     std::vector<int> result = solution(v, 0);
-    EXPECT_EQ(result.size(), 4);
+    ASSERT_EQ(result.size(), 4);
     for (int i=0; i<4; ++i) {
-        ASSERT_EQ(result[i], v[i]);
+        EXPECT_EQ(result[i], v[i]);
+    }
+}
+
+TEST(CyclicRotation, RotateByOne) {
+    int init[] = {1, 2, 3, 4};
+    std::vector<int> v(init, init + sizeof(init)/sizeof(init[0]));
+    std::vector<int> result = solution(v, 1);
+    ASSERT_EQ(result.size(), 4);
+    int expected_result_init[] = {4, 1, 2, 3};
+    std::vector<int> expected_result(expected_result_init, expected_result_init + sizeof(init)/sizeof(init[0]));
+    for (int i=0; i<4; ++i) {
+        EXPECT_EQ(result[i], expected_result[i]);
     }
 }
