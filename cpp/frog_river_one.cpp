4ac32daee8be988204f1c486b59794971daec614
Robert Hardy <robbert@gmail.com>
2016-06-06 12:06:24 +0000

    Refactor out the min_index function.
    All green.

--- cpp/frog_river_one.cpp
+++ cpp/frog_river_one.cpp
@@ -1,11 +1,18 @@
 #include "frog_river_one.hpp"
 
+int min_index(int target, int A[], int N);
+
 int solution(int X, int A[], int N) {
     if (X == 1) {
-        for (int i=0; i<N; i++) {
-            if (A[i] == 1) {
-                return i;
-            }
+        return min_index(1, A, N);
+    }
+    return -1;
+}
+
+int min_index(int target, int A[], int N) {
+    for (int i=0; i<N; i++) {
+        if (A[i] == target) {
+            return i;
         }
     }
     return -1;
