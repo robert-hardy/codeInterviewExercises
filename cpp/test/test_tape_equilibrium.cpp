7345a90d09ee5d3ebc3adc63b49b9f99c7001021
Robert Hardy <robbert@gmail.com>
2016-06-06 15:34:31 +0000

    Add test on case with no solution.
    All green.

--- cpp/test/test_tape_equilibrium.cpp
+++ cpp/test/test_tape_equilibrium.cpp
@@ -39,3 +39,10 @@ TEST(TapeEquilibrium, CheckSumFromDoesNotDamageIterator) {
     ASSERT_EQ(result, 4);
     ASSERT_EQ(*i, 3);
 }
+
+TEST(TapeEquilibrium, NoSolution) {
+    int init[] = {1, 3, 3, 3};
+    std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
+    int result = TapeEquilibrium::solution(A);
+    ASSERT_EQ(result, -1);
+}
