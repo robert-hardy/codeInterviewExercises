30acc66361c20f15211136e7080962702721646c
Robert Hardy <robbert@gmail.com>
2016-06-06 11:42:45 +0000

    Declare solution function and put a red test.
    Test red, wip.

---
+++ cpp/frog_river_one.hpp
@@ -0,0 +1,7 @@
+#ifndef FROGRIVERONE_HPP
+#define FROGRIVERONE_HPP
+
+int solution(int X, int A[], int N);
+
+#endif /* FROGRIVERONE_HPP */
+
---
+++ cpp/test/test_frog_river_one.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "../frog_river_one.hpp"
+
+TEST(FrogRiverOne, GettingStarted) {
+    int init[] = {1, 3, 1, 4, 2, 3, 5, 4};
+    ASSERT_EQ(1, 2);
+}
