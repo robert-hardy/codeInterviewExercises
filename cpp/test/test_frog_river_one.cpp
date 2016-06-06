c3052b3ffa3f222c785447676811f1e405a90049
Robert Hardy <robbert@gmail.com>
2016-06-06 12:11:16 +0000

    Add first test which will need a meaningful implementation.
    Test red, wip.

--- cpp/test/test_frog_river_one.cpp
+++ cpp/test/test_frog_river_one.cpp
@@ -13,3 +13,9 @@ TEST(FrogRiverOne, TestRiverWide1NotFirstIndex) {
     int result = solution(1, A, 8);
     ASSERT_EQ(result, 2);
 }
+
+TEST(FrogRiverOne, TestRiverWide2) {
+    int A[] = {4, 3, 1, 4, 2, 3, 5, 4};
+    int result = solution(2, A, 8);
+    ASSERT_EQ(result, 4);
+}
