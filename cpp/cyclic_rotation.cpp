e1e924f8acfbcb09550a9afbe2cdc5662e5bf384
Robert Hardy <robbert@gmail.com>
2016-06-06 11:17:46 +0000

    Add the expected loop.
    All green.

--- cpp/cyclic_rotation.cpp
+++ cpp/cyclic_rotation.cpp
@@ -5,8 +5,11 @@ std::vector<int> solution(std::vector<int> &A, int K) {
     if (K == 0) {
         return result;
     }
-    int buffer = result.back();
-    result.insert(result.begin(), buffer);
-    result.erase(result.end() - 1);
+    int buffer;
+    for (int i=0; i<K; i++) {
+        buffer = result.back();
+        result.insert(result.begin(), buffer);
+        result.erase(result.end() - 1);
+    }
     return result;
 }
