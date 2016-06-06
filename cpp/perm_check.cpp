a4fc65391352e0ff225be9471c2598f2dfdbf728
Robert Hardy <robbert@gmail.com>
2016-06-06 14:19:11 +0000

    First implementation, based on total sum.
    All green.

--- cpp/perm_check.cpp
+++ cpp/perm_check.cpp
@@ -2,6 +2,15 @@
 
 namespace PermCheck {
     int solution(std::vector<int> &A) {
-        return 0;
+        int N = A.size();
+        int total_sum = 0;
+        std::vector<int>::iterator it = A.begin();
+        for (; it< A.end(); it++) {
+            total_sum += *it;
+        }
+        if (total_sum != N*(N+1)/2) {
+            return 0;
+        }
+        return 1;
     }
 }
--- cpp/test/test_perm_check.cpp
+++ cpp/test/test_perm_check.cpp
@@ -2,8 +2,15 @@
 
 #include "../perm_check.hpp"
 
-TEST(PermCheck, CheckInitialization) {
+TEST(PermCheck, CheckTrueCase) {
     int init[] = {1, 2, 3, 4};
+    std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
+    int result = PermCheck::solution(A);
+    ASSERT_EQ(result, 1);
+}
+
+TEST(PermCheck, CheckFalseCase) {
+    int init[] = {1, 2, 4, 4};
     std::vector<int> A(init, init+sizeof(init)/sizeof(init[0]));
     int result = PermCheck::solution(A);
     ASSERT_EQ(result, 0);
