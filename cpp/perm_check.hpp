dc988c85ae4600e95e7f801efd7b61c23012f774
Robert Hardy <robbert@gmail.com>
2016-06-06 13:59:36 +0000

    Basic files and make for PermCheck exercise.
    Failing test, as confirmation that build works.

---
+++ cpp/perm_check.hpp
@@ -0,0 +1,9 @@
+#ifndef PERMCHECK_HPP
+#define PERMCHECK_HPP
+
+#include <vector>
+
+int solution(std::vector<int> &A);
+
+#endif /* PERMCHECK_HPP */
+
---
+++ cpp/test/test_perm_check.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+#include "../perm_check.hpp"
+
+TEST(PermCheck, GettingStarted) {
+    ASSERT_EQ(1, 2);
+}
