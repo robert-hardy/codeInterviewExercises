73cf51e6804552941f61de43bcfcdb4ff472a3e8
Robert Hardy <robbert@gmail.com>
2016-06-06 14:12:09 +0000

    Minor, modify way we use namespace keyword.
    All green.

--- cpp/perm_check.cpp
+++ cpp/perm_check.cpp
@@ -1,5 +1,7 @@
 #include "perm_check.hpp"
 
-int PermCheck::solution(std::vector<int> &A) {
-    return 0;
+namespace PermCheck {
+    int solution(std::vector<int> &A) {
+        return 0;
+    }
 }
