e37cf2d7b06cde57c84ee52028f89eacc341933b
Robert Hardy <robbert@gmail.com>
2016-06-07 19:54:36 +0000

    Compiles.

--- cs/hello_world.cs
+++ cs/hello_world.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class HelloWorld
+namespace CodeReview
 {
-    static public void Main ()
+    public interface IMarketDataFeed
     {
-        Console.WriteLine ("Hello World!");
+        string Stock { get; }
+        event Action<MarketTick> Tick;
     }
+
+    public interface IIndicator
+    {
+        event Action<double> IndicatorUpdated;
+    }
+
+    public class AverageHalfSpreadIndicator
+    {
+        private List<MarketTick> _lastTicks;
+
+        public AverageHalfSpreadIndicator(IMarketDataFeed marketDataFeed)
+        {
+            marketDataFeed.Tick += OnTick;
+        }
+
+        private void OnTick(MarketTick ticks)
+        {
+            _lastTicks.Add(ticks);
+            if (_lastTicks.Count == 20)
+            {
+                _lastTicks.RemoveAt(0);
+            }
+
+            var start = DateTime.Now;
+            var spreads = _lastTicks.Select(x => 100 * (x.Mid - x.Bid) / x.Bid);
+            Console.WriteLine("Time (computing spreads): " + (DateTime.Now -start).TotalMilliseconds + " ms");
+            var averageSpread = spreads.Average();
+            Console.WriteLine("Time (total): " + (DateTime.Now -start).TotalMilliseconds + " ms");
+            IndicatorUpdated(averageSpread);
+        }
+        public event Action<double> IndicatorUpdated;
+    }
+
+    public class MarketTick
+    {
+        public double Bid { get; set; }
+        public double Ask { get; set; }
+        public double Last { get; set; }
+
+        public double Mid
+        {
+            get { return (Bid + Ask) / 2; }
+        }
+    }
+
+    public class HelloWorld
+    {
+        static public void Main ()
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+
 }
