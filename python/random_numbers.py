26eb6770b76ee0e328c026e15b1846e933064785
Robert Hardy <robbert@gmail.com>
2016-06-09 15:01:37 +0000

    Minor, remove whitespace.

--- python/random_numbers.py
+++ python/random_numbers.py
@@ -1,12 +1,12 @@
 from bisect import bisect_left, bisect_right
 
-def make_right_cts_step_function( step_points, plateaus):
+def make_right_cts_step_function(step_points, plateaus):
     def right_cts_step_function(x):
         i = bisect_right(step_points, x)
         return plateaus[i]
     return right_cts_step_function
 
-def make_left_cts_step_function( step_points, plateaus):
+def make_left_cts_step_function(step_points, plateaus):
     def left_cts_step_function(x):
         i = bisect_left(step_points, x)
         return plateaus[i]
