a3e3aa6612cd14f34c17ffde92a4cea44cc1bc69
Robert Hardy <robbert@gmail.com>
2016-06-09 16:28:51 +0000

    Get rid of the right-cts step function factory and its tests.
    All green.

--- python/test/test_random_numbers.py
+++ python/test/test_random_numbers.py
@@ -3,27 +3,10 @@ import unittest
 from decimal import Decimal
 
 from random_numbers import (
-    make_right_cts_step_function,
     make_left_cts_step_function,
     make_inv_dist_func,
     cumulative_sum
 )
-
-class TestRightCtsStepFunction(unittest.TestCase):
-    def setUp(self):
-        self.func = make_right_cts_step_function(
-            step_points=[60, 70, 80, 90],
-            plateaus='FDCBA'
-        )
-
-    def test_leftmost(self):
-        self.assertEqual(self.func(30), 'F')
-
-    def test_intermediate(self):
-        self.assertEqual(self.func(60), 'D')
-
-    def test_rightmost(self):
-        self.assertEqual(self.func(95), 'A')
 
 class TestLeftCtsStepFunction(unittest.TestCase):
     def setUp(self):
