f665a1cf3ff12beeb6a32626e3135720b0bf5a2e
Robert Hardy <robbert@gmail.com>
2016-06-09 14:42:01 +0000

    Use bisect_left and bisect_right.
    All green.

--- python/random_numbers.py
+++ python/random_numbers.py
@@ -1,11 +1,12 @@
-from bisect import bisect
+from bisect import bisect_left, bisect_right
 
 def step_function(
         x,
         step_points=[60, 70, 80, 90],
         plateaus='FDCBA',
         right_cts=True):
-    i = bisect(step_points, x)
-    if not right_cts:
-        i = max(0, i-1)
+    if right_cts:
+        i = bisect_right(step_points, x)
+    else:
+        i = bisect_left(step_points, x)
     return plateaus[i]
--- python/test/test_random_numbers.py
+++ python/test/test_random_numbers.py
@@ -8,3 +8,11 @@ class Test(unittest.TestCase):
 
     def test_left_cts(self):
         self.assertEqual(step_function(60, right_cts=False), 'F')
+
+    def test_leftmost(self):
+        self.assertEqual(step_function(30), 'F')
+        self.assertEqual(step_function(30, right_cts=False), 'F')
+
+    def test_rightmost(self):
+        self.assertEqual(step_function(95), 'A')
+        self.assertEqual(step_function(95, right_cts=False), 'A')
