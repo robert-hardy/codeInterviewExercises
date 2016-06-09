555a6b0807b4ad276c373df88d91939d7759bdaf
Robert Hardy <robbert@gmail.com>
2016-06-09 15:01:09 +0000

    Remove earlier step function.

--- python/random_numbers.py
+++ python/random_numbers.py
@@ -1,16 +1,5 @@
 from bisect import bisect_left, bisect_right
 
-def step_function(
-        x,
-        step_points=[60, 70, 80, 90],
-        plateaus='FDCBA',
-        right_cts=True):
-    if right_cts:
-        i = bisect_right(step_points, x)
-    else:
-        i = bisect_left(step_points, x)
-    return plateaus[i]
-
 def make_right_cts_step_function( step_points, plateaus):
     def right_cts_step_function(x):
         i = bisect_right(step_points, x)
--- python/test/test_random_numbers.py
+++ python/test/test_random_numbers.py
@@ -1,25 +1,9 @@
 import unittest
 
 from random_numbers import (
-    step_function,
     make_right_cts_step_function,
     make_left_cts_step_function
 )
-
-class Test(unittest.TestCase):
-    def test_right_cts(self):
-        self.assertEqual(step_function(60), 'D')
-
-    def test_left_cts(self):
-        self.assertEqual(step_function(60, right_cts=False), 'F')
-
-    def test_leftmost(self):
-        self.assertEqual(step_function(30), 'F')
-        self.assertEqual(step_function(30, right_cts=False), 'F')
-
-    def test_rightmost(self):
-        self.assertEqual(step_function(95), 'A')
-        self.assertEqual(step_function(95, right_cts=False), 'A')
 
 class TestRightCtsStepFunction(unittest.TestCase):
     def setUp(self):
