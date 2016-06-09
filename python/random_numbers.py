65bf1d54313f14ae508e4d796875f078b1b2f24f
Robert Hardy <robbert@gmail.com>
2016-06-09 15:00:14 +0000

    Add factory for left-cts step functions, with tests.
    All tests green.

--- python/random_numbers.py
+++ python/random_numbers.py
@@ -16,3 +16,9 @@ def make_right_cts_step_function( step_points, plateaus):
         i = bisect_right(step_points, x)
         return plateaus[i]
     return right_cts_step_function
+
+def make_left_cts_step_function( step_points, plateaus):
+    def left_cts_step_function(x):
+        i = bisect_left(step_points, x)
+        return plateaus[i]
+    return left_cts_step_function
--- python/test/test_random_numbers.py
+++ python/test/test_random_numbers.py
@@ -2,7 +2,8 @@ import unittest
 
 from random_numbers import (
     step_function,
-    make_right_cts_step_function
+    make_right_cts_step_function,
+    make_left_cts_step_function
 )
 
 class Test(unittest.TestCase):
@@ -20,10 +21,34 @@ class Test(unittest.TestCase):
         self.assertEqual(step_function(95), 'A')
         self.assertEqual(step_function(95, right_cts=False), 'A')
 
-class TestStepFunctionFactory(unittest.TestCase):
-    def test_make_right_cts_step_function(self):
-        func = make_right_cts_step_function(
+class TestRightCtsStepFunction(unittest.TestCase):
+    def setUp(self):
+        self.func = make_right_cts_step_function(
             step_points=[60, 70, 80, 90],
             plateaus='FDCBA'
         )
-        self.assertEqual(func(60), 'D')
+
+    def test_leftmost(self):
+        self.assertEqual(self.func(30), 'F')
+
+    def test_intermediate(self):
+        self.assertEqual(self.func(60), 'D')
+
+    def test_rightmost(self):
+        self.assertEqual(self.func(95), 'A')
+
+class TestLefCtsStepFunction(unittest.TestCase):
+    def setUp(self):
+        self.func = make_left_cts_step_function(
+            step_points=[60, 70, 80, 90],
+            plateaus='FDCBA'
+        )
+
+    def test_leftmost(self):
+        self.assertEqual(self.func(30), 'F')
+
+    def test_intermediate(self):
+        self.assertEqual(self.func(60), 'F')
+
+    def test_rightmost(self):
+        self.assertEqual(self.func(95), 'A')
