45c835b97504abcd58019e34eed41dcc4098a51a
Robert Hardy <robbert@gmail.com>
2016-06-09 09:04:18 +0000

    Add test to check that random.randint can cope with large integers.
    All green.

--- python/test/test_random_numbers.py
+++ python/test/test_random_numbers.py
@@ -1,8 +1,14 @@
 import unittest
 
+from math import isnan
+from random import randint
+
 from random_numbers import collect_many_random_integers
 
 class Test(unittest.TestCase):
+    def test_big_random_integers(self):
+        self.assertFalse(isnan(randint(0, 10 ** 10)))
+
     def test_basic_randint(self):
         result = collect_many_random_integers()
         self.assertEqual(result, set(range(10)))
