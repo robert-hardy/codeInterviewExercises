e53aa8fce716cfff407ed84c51060f9e924d776e
Robert Hardy <robbert@gmail.com>
2016-06-10 11:13:37 +0000

    Refactor tests.

--- python/test/test_sql_query.py
+++ python/test/test_sql_query.py
@@ -36,6 +36,100 @@ class TestDBInitialize(unittest.TestCase):
         self.assertEqual(quantity, set([1, 1]))
 
 
+def execute_inner_query(conn, today):
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT
+            name,
+            (CASE
+                WHEN
+                    client_order.dispatch_date < date('{now}', '-1 years')
+                    OR
+                    client_order.dispatch_date IS NULL
+                THEN
+                    0
+                ELSE
+                    client_order.quantity
+            END
+            ) as order_size_in_last_year
+        FROM
+            product LEFT OUTER JOIN client_order
+            ON product.product_id = client_order.product_id
+        WHERE
+            available_from < date('{now}', '-1 months');
+    """.format(now=today.isoformat()))
+    results = [ (r['name'], r['order_size_in_last_year']) for r in cur.fetchall() ]
+    return results
+
+
+class TestInnerQuery(unittest.TestCase):
+    # Checks that we can produce a list, for each not-new book
+    # of the sizes of orders in the last year.
+    # These results will be aggregated in the main query.
+    def setUp(self):
+        self.conn = initialize_db(':memory:')
+        product_rows = [
+            (101, "book1", 91, "2016-05-15"),
+            (102, "book2", 92, "2017-06-01"),
+            (103, "book3", 93, "2016-03-01"),
+            (104, "book4", 94, "2014-06-01"),
+            (105, "book5", 95, "2015-06-01")
+        ]
+        populate_product_table(self.conn, product_rows)
+        self.today = date(2016, 6, 10)
+
+    def test_no_orders_at_all(self):
+        # Basically gives us a list of the not-new books
+        results = execute_inner_query(self.conn, self.today)
+        self.assertEqual(results, [
+            (u'book3', 0),
+            (u'book4', 0),
+            (u'book5', 0)
+        ])
+
+    def test_one_new_book_with_one_order(self):
+        order_rows = [
+            (1000, 101, 1,  91, "2016-04-01")
+        ]
+        populate_order_table(self.conn, order_rows)
+        results = execute_inner_query(self.conn, self.today)
+        self.assertEqual(results, [
+            (u'book3', 0),
+            (u'book4', 0),
+            (u'book5', 0)
+        ])
+
+    def test_one_not_new_book_with_one_order(self):
+        order_rows = [
+            (1000, 103, 1,  91, "2016-04-01")
+        ]
+        populate_order_table(self.conn, order_rows)
+        results = execute_inner_query(self.conn, self.today)
+        self.assertEqual(results, [
+            (u'book3', 1),
+            (u'book4', 0),
+            (u'book5', 0)
+        ])
+
+    def test_with_two_good_orders_for_book3(self):
+        order_rows = [
+            (1000, 101, 1,  91, "2016-04-01"),
+            (1001, 103, 1,  92, "2016-05-01"),
+            (1002, 101, 10, 93, "2015-05-01"),
+            (1003, 104, 11, 94, "2016-01-01"),
+            (1004, 105, 11, 95, "2014-06-01"),
+            (1005, 103, 1,  92, "2016-05-01")
+        ]
+        populate_order_table(self.conn, order_rows)
+        results = execute_inner_query(self.conn, self.today)
+        self.assertEqual(results, [
+            (u'book3', 1),
+            (u'book3', 1),
+            (u'book4', 11),
+            (u'book5', 0)
+        ])
+
+
 def full_populate_tables(conn):
     product_rows = [
         (101, "book1", 91, "2016-05-15"),
@@ -60,116 +154,22 @@ def full_populate_tables(conn):
 class TestQuery(unittest.TestCase):
     def setUp(self):
         self.today = date(2016, 6, 10)
-
-    def test(self):
-        conn = initialize_db(':memory:')
-        full_populate_tables(conn)
-        results = get_books_that_are_not_selling_well(conn, self.today)
-        self.assertEqual(results, [
-            {'product_id': 103, 'total_orders_in_last_year': 2},
-            {'product_id': 105, 'total_orders_in_last_year': 0}
-        ])
-
-
-class TestQueryBeforeGroupBy(unittest.TestCase):
-    # Checks that we can produce a list, for each not-new book
-    # of the sizes of orders in the last year.
-    # These results will be grouped in the main query.
-    def execute_query(self):
-        cur = self.conn.cursor()
-        cur.execute("""
-            SELECT
-                name,
-                (CASE
-                    WHEN
-                        client_order.dispatch_date < date('{now}', '-1 years')
-                        OR
-                        client_order.dispatch_date IS NULL
-                    THEN
-                        0
-                    ELSE
-                        client_order.quantity
-                END
-                ) as order_size_in_last_year
-            FROM
-                product LEFT OUTER JOIN client_order
-                ON product.product_id = client_order.product_id
-            WHERE
-                available_from < date('{now}', '-1 months');
-        """.format(now=self.today.isoformat()))
-        results = [ (r['name'], r['order_size_in_last_year']) for r in cur.fetchall() ]
-        return results
-
-    def setUp(self):
         self.conn = initialize_db(':memory:')
-        product_rows = [
-            (101, "book1", 91, "2016-05-15"),
-            (102, "book2", 92, "2017-06-01"),
-            (103, "book3", 93, "2016-03-01"),
-            (104, "book4", 94, "2014-06-01"),
-            (105, "book5", 95, "2015-06-01")
-        ]
-        populate_product_table(self.conn, product_rows)
-        self.today = date(2016, 6, 10)
-
-    def test_no_orders_at_all(self):
-        # Basically gives us a list of the not-new books
-        results = self.execute_query()
-        self.assertEqual(results, [
-            (u'book3', 0),
-            (u'book4', 0),
-            (u'book5', 0)
-        ])
-
-    def test_one_new_book_with_one_order(self):
-        order_rows = [
-            (1000, 101, 1,  91, "2016-04-01")
-        ]
-        populate_order_table(self.conn, order_rows)
-        results = self.execute_query()
-        self.assertEqual(results, [
-            (u'book3', 0),
-            (u'book4', 0),
-            (u'book5', 0)
-        ])
-
-    def test_one_not_new_book_with_one_order(self):
-        order_rows = [
-            (1000, 103, 1,  91, "2016-04-01")
-        ]
-        populate_order_table(self.conn, order_rows)
-        results = self.execute_query()
-        self.assertEqual(results, [
-            (u'book3', 1),
-            (u'book4', 0),
-            (u'book5', 0)
-        ])
-
-    def test_with_two_good_orders_for_book3(self):
-        order_rows = [
-            (1000, 101, 1,  91, "2016-04-01"),
-            (1001, 103, 1,  92, "2016-05-01"),
-            (1002, 101, 10, 93, "2015-05-01"),
-            (1003, 104, 11, 94, "2016-01-01"),
-            (1004, 105, 11, 95, "2014-06-01"),
-            (1005, 103, 1,  92, "2016-05-01")
-        ]
-        populate_order_table(self.conn, order_rows)
-        results = self.execute_query()
-        self.assertEqual(results, [
-            (u'book3', 1),
-            (u'book3', 1),
-            (u'book4', 11),
-            (u'book5', 0)
-        ])
-
-    def test_using_orders_in_test_on_main_query(self):
         full_populate_tables(self.conn)
-        results = self.execute_query()
+
+    def test_inner_query(self):
+        results = execute_inner_query(self.conn, self.today)
         self.assertEqual(results, [
             (u'book3', 1),
             (u'book3', 1),
             (u'book4', 5),
             (u'book4', 6),
             (u'book5', 0)
+        ])
+
+    def test_main_query(self):
+        results = get_books_that_are_not_selling_well(self.conn, self.today)
+        self.assertEqual(results, [
+            {'product_id': 103, 'total_orders_in_last_year': 2},
+            {'product_id': 105, 'total_orders_in_last_year': 0}
         ])
