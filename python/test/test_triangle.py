1efa27c975abe1ac7cba571320687db7ed2fb675
Robert Hardy <robbert@gmail.com>
2016-06-14 15:13:05 +0000

    Put the previous, more obvious itertools solution back and check that my lean version gives the same result over quite a few random trials.
    All green.

--- python/test/test_triangle.py
+++ python/test/test_triangle.py
@@ -1,6 +1,8 @@
 import unittest
 
-from triangle import solution
+from random import randint
+
+from triangle import solution, solution2
 
 class Test(unittest.TestCase):
     def test(self):
@@ -17,3 +19,11 @@ class Test(unittest.TestCase):
         A = [ 1 ] * 100
         result = solution(A)
         self.assertEqual(result, 1)
+
+class TestTwoApproachesAgree(unittest.TestCase):
+    def test_random_samples(self):
+        for i in range(5000):
+            A = [ randint(1, 50) for i in range(200) ]
+            result = solution(A)
+            result2 = solution2(A)
+            self.assertEqual(result, result2)
--- python/triangle.py
+++ python/triangle.py
@@ -6,3 +6,9 @@ def solution(A):
         if a + b > c:
             return 1
     return 0
+
+def solution2(A):
+    for a, b, c in itertools.combinations(A, 3):
+        if a+b>c and a+c>b and b+c>a:
+            return 1
+    return 0
