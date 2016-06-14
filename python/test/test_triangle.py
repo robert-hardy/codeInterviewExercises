4c13c061aed194b67735b1e8680cea820fcd1487
Robert Hardy <robbert@gmail.com>
2016-06-14 14:34:34 +0000

    Add test to see time taken.
    All green, but getting slow.

--- python/test/test_triangle.py
+++ python/test/test_triangle.py
@@ -7,3 +7,8 @@ class Test(unittest.TestCase):
         A = [ 1, 2, 3, 4, 5 ]
         result = solution(A)
         self.assertEqual(result, 1)
+
+    def test_no_triangle(self):
+        A = [ 0 ] * 1000
+        result = solution(A)
+        self.assertEqual(result, 0)
