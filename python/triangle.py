3b9411b7e83c3f4290099ecedd57905d263f70f5
Robert Hardy <robbert@gmail.com>
2016-06-14 14:28:40 +0000

    Use itertools.combinations.
    All green.

--- python/triangle.py
+++ python/triangle.py
@@ -1,2 +1,7 @@
+import itertools
+
 def solution(A):
-    pass
+    for a, b, c in itertools.combinations(enumerate(A), 3):
+        if a[1] + b[1] > c[1]:
+            return (a[0], b[0], c[0])
+    return False
